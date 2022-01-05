e0861c9221a9291e1a75f1cb790d2eecc85a0b1a
nepito <braulio.rojas@islas.org.mx>
2022-01-04 20:28:55 -0700

    Add first tests

---
+++ tests/testthat/test_nothing.R
@@ -0,0 +1,8 @@
+describe("Get version of the module", {
+  it("The version is 0.1.0", {
+    expected_version <- c("0.1.0")
+    obtained_version <- packageVersion("EconEngy")
+    version_are_equal <- expected_version == obtained_version
+    expect_true(version_are_equal)
+  })
+})
